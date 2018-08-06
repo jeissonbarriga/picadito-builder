9746c821203d8f0fb3f9a822ce0e5457fa3b7e36
jeisson33 <stuartjabq@gmail.com>
2018-08-05 21:37:54 -0500

    New-player form field, add player and generate teams basic functionality.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,47 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
+import { Player } from "./models/player.model";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {}
+export class AppComponent {
+  players: Player[] = [];
+  newPlayer: Player = new Player();
+
+  team1: Player[] = [];
+  team2: Player[] = [];
+  aditionalPlayer: Player;
+
+  addNewPlayer() {
+    if (this.newPlayer.name) {
+      this.players.push(this.newPlayer);
+      this.newPlayer = new Player();
+    }
+  }
+
+  generateTeams() {
+    this.resetTeams();
+    let remainingPlayers = [...this.players];
+    while (remainingPlayers.length > 0) {
+      let playerIndex = Math.floor(Math.random() * remainingPlayers.length);
+      let player = remainingPlayers[playerIndex];
+      this.assignTeam(player, remainingPlayers.length);
+      remainingPlayers.splice(playerIndex, 1);
+    }
+  }
+
+  resetTeams() {
+    this.team1 = [];
+    this.team2 = [];
+  }
+
+  assignTeam(player: Player, remaining: number) {
+    if (remaining % 2 === 0) {
+      this.team1.push(player);
+    } else {
+      this.team2.push(player);
+    }
+  }
+}
